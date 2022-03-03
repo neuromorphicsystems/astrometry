80b9bd10355e27cc1edd3808a1eaba6caf0be7b4
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2022-03-03 18:00:00 +1100

    Bump the version

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ if compiler.startswith("Clang") or compiler.startswith("GCC"):
 
 setuptools.setup(
     name="astrometry",
-    version="0.0.2",
+    version="1.0.0",
     url="https://github.com/neuromorphicsystems/astrometry",
     author="ICNS, Alexandre Marcireau",
     author_email="alexandre.marcireau@gmail.com",
